8feb97e2de9a4f172b271f7c8878f01dc46b09ad
Octahedron-apple <bhavya1singh1@gmail.com>
2026-03-25 15:54:45 +0530

    Remove unnecessary blank lines in MainActivity.java

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.quizapp;
-
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
